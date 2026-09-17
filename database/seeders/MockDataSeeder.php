1537623c854e1928601f9e85a631a563d5604eff
NietThijmen <53520119+NietThijmen@users.noreply.github.com>
2026-09-17 15:15:51 +0200

    feat(seed): add MockDataSeeder for seeding test data

---
+++ database/seeders/MockDataSeeder.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enums\ChangelogStatus;
+use App\Models\Changelog;
+use App\Models\ChangelogChange;
+use App\Models\Package;
+use App\Models\Registry;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class MockDataSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $packagist = Registry::create([
+            'name' => 'Packagist',
+            'url' => 'https://repo.packagist.org',
+            'auth_type' => 'none',
+        ]);
+
+        Registry::create([
+            'name' => 'WPackagist',
+            'url' => 'https://wpackagist.org',
+            'auth_type' => 'none',
+        ]);
+
+
+        Package::create([
+            'name' => 'laravel/laravel',
+            'registry_id' => $packagist->id,
+            'is_active' => true,
+            'current_version' => '13.0.0'
+        ]);
+
+        Package::create([
+            'name' => 'laravel/framework',
+            'registry_id' => $packagist->id,
+            'is_active' => true,
+            'current_version' => '13.0.0'
+        ]);
+
+        Changelog::factory(5)->create([
+            'package_id' => 1,
+            'status' => ChangelogStatus::Checked
+        ]);
+
+        ChangelogChange::factory(50)->create([
+            'changelog_id' => 1
+        ]);
+
+
+
+    }
+}
